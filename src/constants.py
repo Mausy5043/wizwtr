e49928d90f10914fabe0f9612498c5fd37a9c33f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-12-25 12:44:57 +0100

    use less confusing key

--- src/constants.py
+++ src/constants.py
@@ -76,7 +76,7 @@ WIZ_WTR: dict = {
         "sample_time": "yyyy-mm-dd hh:mm:ss",
         "sample_epoch": 0,
         "water": 0, # L
-    "null": 891.719,    # m3
+    "offset": 891.719, # m3
     "calibration": {
         "2024-12-25 11:00:00": 0.000    # correction in m3
         },
