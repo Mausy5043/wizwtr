ac1e59c24a87410f8426f1d00fa60538d64e1f36
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-12-25 13:10:36 +0100

    update compaction

--- src/libwizwtr.py
+++ src/libwizwtr.py
@@ -143,8 +143,7 @@ class WizWTR_v1:  # pylint: disable=too-many-instance-attributes
         df_out = df.resample("15min", label="right").max()
         # df_mean = df.resample("15min", label="right").mean()
 
-        df_out["powerin"] = df_out["powerin"].astype(int)
-        df_out["powerout"] = df_out["powerout"].astype(int)
+        df_out["water"] = df_out["water"].astype(int)
         # recreate column 'sample_time' that was lost to the index
         df_out["sample_time"] = df_out.index.to_frame(name="sample_time")
         df_out["sample_time"] = df_out["sample_time"].apply(_convert_time_to_text)
